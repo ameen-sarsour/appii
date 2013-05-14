79bc7396550ed89702a486b091f354ae97de50ee
ameen <ameen@localhost.localdomain>
2013-05-14 17:43:57 +0300

    add font, Uthman Taha and Uthman Taha Bold

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -4,8 +4,8 @@
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<meta name="language" content="en" />
-
 	<title><?php echo CHtml::encode($this->pageTitle); ?></title>
+	 <link rel="stylesheet" type="text/css" href="/css/ourstyle.css">
 </head>
 
 <body>
